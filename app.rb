72d7099a2736d60ea8f26eb12349977077efe4d1
Canino Developments <ian.canino@gmail.com>
2018-05-16 17:33:06 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,11 +1,13 @@
 require_relative 'config/environment'
 
+# This controller describes the connection between
+# erb and sinatra
 class App < Sinatra::Base
-
   get '/food_form' do
     erb :food_form
   end
 
-  # Add your post route and action below
-
+  post '/food' do
+    "My name is #{params[:name]}, and I love #{params[:favorite_food]}"
+  end
 end
